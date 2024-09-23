3625d3d2c87370614e140368183061ccdcb7def6
anssakthi <sakthiprakash.r@ansys.com>
2024-09-23 15:12:02 +0530

    integrations: Change Git commit url format.

--- zulip/integrations/git/zulip_git_config.py
+++ zulip/integrations/git/zulip_git_config.py
@@ -1,13 +1,12 @@
 #
-from typing import Dict, Optional
+from typing import Dict, Text, Optional
 
 # Name of the stream to send notifications to, default is "commits"
-STREAM_NAME = "commits"
+STREAM_NAME = 'commits'
 
 # Change these values to configure authentication for the plugin
 ZULIP_USER = "git-bot@example.com"
 ZULIP_API_KEY = "0123456789abcdef0123456789abcdef"
-
 
 # commit_notice_destination() lets you customize where commit notices
 # are sent to with the full power of a Python function.
@@ -20,30 +19,21 @@ ZULIP_API_KEY = "0123456789abcdef0123456789abcdef"
 # Returns a dictionary encoding the stream and subject to send the
 # notification to (or None to send no notification).
 #
-# The default code below will send every commit pushed to "main" to
+# The default code below will send every commit pushed to "master" to
 # * stream "commits"
-# * topic "main"
+# * topic "master"
 # And similarly for branch "test-post-receive" (for use when testing).
-def commit_notice_destination(repo: str, branch: str, commit: str) -> Optional[Dict[str, str]]:
-    if branch in ["main", "master", "test-post-receive"]:
-        return dict(stream=STREAM_NAME, subject=f"{branch}")
+def commit_notice_destination(repo: Text, branch: Text, commit: Text) -> Optional[Dict[Text, Text]]:
+    if branch in ["master", "test-post-receive"]:
+        return dict(stream  = STREAM_NAME,
+                    subject = "%s" % (branch,))
 
     # Return None for cases where you don't want a notice sent
     return None
 
-
-# Modify this function to change how commits are displayed; the most
-# common customization is to include a link to the commit in your
-# graphical repository viewer, e.g.
-#
-# return '!avatar(%s) [%s](https://example.com/commits/%s)\n' % (author, subject, commit_id)
-def format_commit_message(author: str, subject: str, commit_id: str) -> str:
-    return f"!avatar({author}) {subject}\n"
-
-
 ## If properly installed, the Zulip API should be in your import
 ## path, but if not, set a custom path below
-ZULIP_API_PATH: Optional[str] = None
+ZULIP_API_PATH = None
 
 # Set this to your Zulip server's API URI
 ZULIP_SITE = "https://zulip.example.com"
